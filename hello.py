566a57544fdf2d395808680a6389d0845ffffe63
Tony Shouse <redpoint13@gmail.com>
2024-01-26 13:56:05 -0700

    [initial commit]

---
+++ hello.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+
+# import modules used here -- sys is a very standard one
+import sys
+
+
+# Defines a "repeat" function that takes 2 arguments.
+def repeat(s, exclaim):
+    """
+    Returns the string 's' repeated 3 times.
+    If exclaim is true, add exclamation marks.
+    """
+
+    result = s * 3 # can also use "s * 3" which is faster (Why?)
+    if exclaim:
+        result = result + '!!!'
+    return result
+
+
+
+# Gather our code in a main() function
+def main():
+    # print('Hello there', sys.argv[1])
+    # Command line args are in sys.argv[1], sys.argv[2] ...
+    # sys.argv[0] is the script name itself and can be ignored
+    print(repeat('Yay', False))      ## YayYayYay
+    print(repeat('Woo Hoo', True))   ## Woo HooWoo HooWoo Hoo!!!
+
+# Standard boilerplate to call the main() function to begin
+# the program.
+if __name__ == '__main__':
+    main()
