5037711099a7cbf945d1708306479c58ea9c72a9
Tony Shouse <redpoint13@gmail.com>
2024-01-26 14:11:15 -0700

    pdate type hints and modify string concatenation for better performance.
    Commit Description:
     - This MR adds type hints to the function, updates the string concatenation to improve performance, and removes unnecessary comments.

--- hello.py
+++ hello.py
@@ -3,6 +3,15 @@
 # import modules used here -- sys is a very standard one
 import sys
 
+"""
+type hints:
+Example:
+    def is_positive(n: int) -> bool:
+
+Means:
+    The argument `n` is an `int` and the return value is a `bool`
+"""
+
 
 # Defines a "repeat" function that takes 2 arguments.
 def repeat(s, exclaim):
@@ -11,11 +20,10 @@ def repeat(s, exclaim):
     If exclaim is true, add exclamation marks.
     """
 
-    result = s * 3 # can also use "s * 3" which is faster (Why?)
+    result = s * 3  # can also use "s * 3" which is faster (Why?)
     if exclaim:
-        result = result + '!!!'
+        result = result + "!!!"
     return result
-
 
 
 # Gather our code in a main() function
@@ -23,10 +31,11 @@ def main():
     # print('Hello there', sys.argv[1])
     # Command line args are in sys.argv[1], sys.argv[2] ...
     # sys.argv[0] is the script name itself and can be ignored
-    print(repeat('Yay', False))      ## YayYayYay
-    print(repeat('Woo Hoo', True))   ## Woo HooWoo HooWoo Hoo!!!
+    print(repeat("Yay", False))  ## YayYayYay
+    print(repeat("Woo Hoo", True))  ## Woo HooWoo HooWoo Hoo!!!
+
 
 # Standard boilerplate to call the main() function to begin
 # the program.
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
